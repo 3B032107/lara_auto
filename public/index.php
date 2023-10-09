096d08f3f1c8d3e4d1f48501e53627da8facb798
Jia-Ju Liao <3b032107@gm.student.ncut.edu.tw>
2023-10-09 20:22:56 +0800

    change Logger name

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 // create a log channel
-$log = new Logger('name');
+$log = new Logger('WISD');
 $log->pushHandler(new StreamHandler('log/my.log', Level::Warning));
 
 // add records to the log
