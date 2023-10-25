a04239ec3e849b8be9bc36ee8772ae6bc409c437
M514-T\admin <@>
2023-10-25 09:37:41 +0800

    更改.log路徑

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@ use Monolog\Handler\StreamHandler;
 
 //create a log channel
 $log = new Logger('WISD');
-$log->pushHandler(new StreamHandler('log/my.log', Level::Warning));
+$log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
 
 //add records to the log
 $log->warning('Foo');
